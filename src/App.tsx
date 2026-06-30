1d3654d9ff130bb81268a2adc4480972eeeb3270
praneethbabu <praneethbabu2003@gmail.com>
2026-06-30 18:49:47 +0530

    intial commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '100px' }}>
       <h1>My React App</h1>
-      <p>Successfully deployed using AWS CodePipeline and CodeBuild 🚀</p>
+      <p>Successfully deployed using AWS CodePipeline and CodeBuild</p>
     </div>
   )
 }
